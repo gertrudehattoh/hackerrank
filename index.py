819af5e6849e50a9c50e1138cf41dc88c76094aa
Gertrude Hattoh <91732778+gertrudehattoh@users.noreply.github.com>
2023-03-16 16:12:18 +0000

    Update index.py

--- index.py
+++ index.py
@@ -6,3 +6,17 @@
 #If  is even and greater than , print Not Weird
 
 
+
+#solution
+n = input('type a number' )
+
+if n%2==1:
+  print('Weird')
+if n in range(2,5) and n%2==0:
+  print('Not Weird')
+if n in range(5,21) and n%2==0:
+  print('weird')
+if n>20 and n%2==0:
+  print('Not Weird')
+
+
